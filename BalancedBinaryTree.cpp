c2f2bc580e7b1a8f18e125fdbf827bc638de1a92
Kevinlymine <1024088134@qq.com>
2016-08-10 17:32:35 +0800

    Balanced binary tree

---
+++ BalancedBinaryTree.cpp
@@ -0,0 +1,42 @@
+// Given a binary tree, determine if it is height-balanced.
+
+// For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+/**
+ * 求是否是平衡二叉树
+ * 思路：首先要知道如何求每个节点的高度
+ * 然后判断每个节点的左子树和右子树高度是否满足平衡二叉树的条件
+ * 如果高度满足, 再判断每个子节点是不是平衡二叉树(很重要, 因为二叉树的平衡, 不仅仅是在根节点的高度平衡, 而是所有节点！！！)
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(!root)
+            return true;
+        int rh = treeHeight(root->right);
+        int lh = treeHeight(root->left);
+        if(abs(rh - lh) <= 1)
+            return (isBalanced(root->right) && isBalanced(root->left));
+        else 
+            return false;
+            
+    }
+    
+    int treeHeight(TreeNode* root) {
+        if(!root)
+            return 0;
+        int l = treeHeight(root->left) + 1;
+        int r = treeHeight(root->right) + 1;
+        return l > r ? l : r;
+    }
+};
