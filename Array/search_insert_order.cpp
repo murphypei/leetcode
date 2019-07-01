466fafc62bf04a6893911ca7530bc1427888fdaf
murphypei <murphypei47@gmail.com>
2019-07-01 10:03:41 +0800

    add gitignore

---
+++ Array/search_insert_order.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+class Solution
+{
+public:
+    int searchInsert(std::vector<int> &nums, int target)
+    {
+        int left = 0, right = nums.size() - 1, mid;
+        while (left <= right)
+        {
+            mid = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
