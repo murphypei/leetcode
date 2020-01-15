03bb5c6396ef38e47b4302aa28cfd0a7dfcc4c5b
murphypei <murphypei47@gmail.com>
2020-01-15 11:28:40 +0800

    add new solutions

---
+++ subsets.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+
+        vector<vector<int>> res;
+        vector<int> out;
+        helper(0, nums, out, res);
+        return res;
+    }
+
+    void helper(int n, const vector<int> &nums, vector<int> out, vector<vector<int>> &res)
+    {
+        res.push_back(out);
+        if (out.size() == nums.size())
+        {
+            return;
+        }
+        for (int i = n; i < nums.size(); ++i)
+        {
+            out.push_back(nums[i]);
+            helper(i + 1, nums, out, res);
+            out.pop_back();
+        }
+    }
+};
